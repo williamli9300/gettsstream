f765f91b9b15f5143e30cf6c67fc800e8a2adf80
williamli9300 <49330381+williamli9300@users.noreply.github.com>
2023-06-06 19:28:33 -0400

    Create getfiles.py

---
+++ getfiles.py
@@ -0,0 +1,72 @@
+import wget, ssl, time, os, subprocess, shutil
+
+def merge(sfn):
+    print("merging ts files...")
+    final_path = "./" + sfn + ".ts"
+    ts_dir = "./" + sfn
+    with open(final_path, 'wb') as merged:
+        for ts_file in os.listdir(ts_dir):
+            print("  > " + ts_file)
+            filepath = ts_dir + "/" + ts_file
+            with open(filepath, 'rb') as mergefile:
+                shutil.copyfileobj(mergefile, merged)
+    print("----- merging OK. -----")
+
+def generate_list(u, sn, en, fn, wl):
+    print("generating url list...")
+    time.sleep(0.25)
+    l = []
+    for i in range(sn, en +1):
+        s = u + str(i) + ".ts\n"
+        l.append(s)
+    sfn = fn.replace(".txt", "")
+    sfn = sfn.replace(".ts", "")
+
+    if wl.upper() != "N":
+        print("writing url list...")
+        time.sleep(0.25)
+        path = "./" + sfn + ".txt"
+        print("list path: " + path)
+        time.sleep(0.25)
+        with open(path, "w") as f:
+            for i in l:
+                f.write(i)
+    print("----- url list OK. continuing to downloads... -----")
+    time.sleep(0.25)
+    return l, sfn
+
+def download_files(l, u, sfn):
+    dl = input("WARNING: SSL verification disabled. Continue? default=Yes, enter N for no\n")
+    if dl.upper == "N":
+        return False
+    else:
+        print("downloading files...")
+        time.sleep(0.25)
+        path = "./" + sfn
+        os.makedirs(path, exist_ok=True)
+        ssl._create_default_https_context = ssl._create_unverified_context
+        fn_prefix_list = u.split("/")
+        fn_prefix = fn_prefix_list[-1]
+        for i in range(len(l)):
+            filename = "./" + sfn + "/" + (str(i+1)).rjust(5, "0") + ".ts"
+            print("    > writing " + filename + "...")
+            wget.download(l[i], out=filename)
+        print("----- downloads OK. continuing to merging... -----")
+        merge(sfn)
+        return True
+
+u = str(input("please enter the URL prefix (before number and '.ts') \n"))
+sn = int(input("please enter the first video number \n"))
+en = int(input("please enter the last video number \n"))
+fn = str(input("please enter file name\n"))
+wl = str(input("write file list? default=Yes, enter N for no\n"))   
+
+print("----- inputs OK. continuing to url list generation... -----")
+time.sleep(0.25)
+l, sfn = generate_list(u, sn, en, fn, wl)
+dl = download_files(l, u, sfn)
+
+if dl == False:
+    input('Operation cancelled. Press <ENTER> to continue')
+else:
+    input('Done. Press <ENTER> to continue')
