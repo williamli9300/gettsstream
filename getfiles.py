71b52f36f68eaa516bb652d7705cc3db9af9c4c7
Will Li <49330381+williamli9300@users.noreply.github.com>
2024-09-23 00:30:06 -0400

    Update getfiles.py

--- getfiles.py
+++ getfiles.py
@@ -1,4 +1,4 @@
-# getTSstream v0.5 - https://github.com/williamli9300/gettsstream
+# getTSstream v0.7 - https://github.com/williamli9300/gettsstream
 
 import wget, ssl, time, os, subprocess, shutil
 
@@ -16,27 +16,28 @@ def merge(sfn):
 
 def generate_list(u, sn, en, fn, wl):
     print("generating url list...")
-    time.sleep(0.05)
+    time.sleep(0.1)
     l = []
     for i in range(sn, en +1):
         s = u + str(i) + ".ts\n"
-        l.append(s + ",,," + str(i))
+        l.append(s)
     sfn = fn.replace(".txt", "")
     sfn = sfn.replace(".ts", "")
 
     if wl.upper() != "N":
         print("writing url list...")
-        time.sleep(0.05)
-        path = "./" + sfn
-        os.makedirs(path, exist_ok=True)
-        listpath = path + "/" + sfn + ".txt"
-        print("list path: " + listpath)
-        time.sleep(0.05)
-        with open(listpath, "w") as f:
+        time.sleep(0.1)
+        folderpath = "./" + sfn
+        os.makedirs(folderpath, exist_ok=True)
+        path = folderpath + "/" + sfn + ".txt"
+
+        print("list path: " + path)
+        time.sleep(0.1)
+        with open(path, "w") as f:
             for i in l:
                 f.write(i)
     print("----- url list OK. continuing to downloads... -----")
-    time.sleep(0.1)
+    time.sleep(0.25)
     return l, sfn
 
 def download_files(l, u, sfn):
@@ -45,31 +46,32 @@ def download_files(l, u, sfn):
         return False
     else:
         print("downloading files...")
-        time.sleep(0.05)
+        time.sleep(0.1)
+        path = "./" + sfn
+        os.makedirs(path, exist_ok=True)
         ssl._create_default_https_context = ssl._create_unverified_context
         fn_prefix_list = u.split("/")
         fn_prefix = fn_prefix_list[-1]
         for i in range(len(l)):
-            line = l[i].split(",,,")
             try:
-                filename = "./" + sfn + "/" + (line[1]).rjust(5, "0") + ".ts"
+                filename = "./" + sfn + "/" + (str(i+1)).rjust(5, "0") + ".ts"
                 print("    > writing " + filename + "...")
-                wget.download(line[0], out=filename)
+                wget.download(l[i], out=filename)
             except Exception as e:
-                print("##### Error thrown with URL " + (line[1]).rjust(5, "0") + ". moving on...")
+                print("##### Error thrown with URL " + (str(i+1)).rjust(5, "0") + ". moving on...")
                 i+=1
         print("----- downloads OK. continuing to merging... -----")
         merge(sfn)
         return True
 
 u = str(input("please enter the URL prefix (before number and '.ts') \n"))
-sn = int(input("please enter the first video number \n"))
-en = int(input("please enter the last video number \n"))
+sn = int(input("please enter the number of the first *.ts file \n"))
+en = int(input("please enter the number of the last *.ts file \n"))
 fn = str(input("please enter file name\n"))
 wl = str(input("write file list? default=Yes, enter N for no\n"))   
 
 print("----- inputs OK. continuing to url list generation... -----")
-time.sleep(0.15)
+time.sleep(0.1)
 l, sfn = generate_list(u, sn, en, fn, wl)
 dl = download_files(l, u, sfn)
 
